c0db216f87c7b2a0721459fefa6f525c03dd5697
Setie Louis Adjambri <seite-louis.adjambri@consultant.vodafoneomnitel.it>
2023-05-12 14:16:15 +0200

    add next and previous btn

--- js/lesson_page_handler.js
+++ js/lesson_page_handler.js
@@ -1,27 +1,2 @@
 
 
-const firstPageContentGetter = function (endpoint, callback){
-
-    lessonPageHandler(endpoint, callback);
-}
-
-function lessonPageHandler(endpoint, callback){
-
-    caller(
-        endpoint,
-        'GET',
-        (data) => pageContent(data, callback),
-        (error) => console.log(error)
-    );
-
-}
-
-
-const pageContent = function(data, callback){
-
-    data.json().then( d => {
-
-        callback(utf8Base64Decoder(d.content))
-    })
-}
-
--- js/pages_loader.js
+++ js/pages_loader.js
@@ -1,21 +1,95 @@
+let pages_metadata = [];
 
-const pagesLoader = function (content, prevPage, nextPage){
+function create_main_div() {
 
     let div = createDiv("");
-
-    div.innerHTML = md.render(content);
 
     div.setAttribute("id", "main_div")
     div.setAttribute("class", "markdown-body")
 
-    let section = document.getElementById("main_section")
-    //clean up
-    section.innerHTML='';
-    section.append(div);
+    return div;
 }
 
-function pageLoadHandler(pagesMetadata){
+function createBtn(index, value) {
+    if ( index < 0|| index >= pages_metadata.length)
+        return '';
+    let button  = document.createElement('button')
+    button.setAttribute("class", "send-button")
+    button.setAttribute("role", "button")
+    button.setAttribute("onclick", "singlePageLoder('"+ index +"')")
 
-    firstPageContentGetter(pagesMetadata[0].link, (mdContent) => { pagesLoader(mdContent)})
+    button.innerHTML = value;
+
+    return button;
+}
+
+
+function btnCreator(index) {
+
+    let nextBtn = createBtn(parseInt(index) + 1, "Next")
+    let previous = createBtn(parseInt(index)- 1, "Previous")
+
+    let btnRow = createDiv("b13");
+
+    btnRow.append(previous)
+    btnRow.append(nextBtn)
+
+    return btnRow;
+}
+
+const pagesLoader = function (content, index){
+
+    //main div
+    let main_div = create_main_div()
+    main_div.innerHTML = md.render(content);
+
+    let  btnRow = btnCreator(index)
+
+    let section = document.getElementById("main_section")
+
+    //remove all child
+    section.innerHTML='';
+
+    section.append(main_div)
+    section.append(btnRow)
+}
+
+function pageLoadHandler(data){
+
+    pages_metadata = data;
+    singlePageLoder(0)
+}
+
+function singlePageLoder(index){
+
+    if (index < 0 || index >= pages_metadata.length)
+        return;
+
+    getPageContent(
+        pages_metadata[index].link,
+        (md_content) => pagesLoader(md_content, index)
+    )
+}
+
+
+//Content retrive handler
+
+function getPageContent(endpoint, callback){
+
+    caller(
+        endpoint,
+        'GET',
+        (data) => pageContent(data, callback),
+        (error) => console.log(error)
+    );
+
+}
+
+const pageContent = function(data, callback){
+
+    data.json().then( d => {
+
+        callback(utf8Base64Decoder(d.content))
+    })
 }
 
